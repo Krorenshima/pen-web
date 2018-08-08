2dbb5d93eb7386aa20ec025ad5aad64dded2430d
Krorenshima <official.chubbyroo@gmail.com>
2018-08-08 04:24:10 -0700

    Changed err
    Changed the error msg style

--- tests/global/err.js
+++ tests/global/err.js
@@ -1,7 +1,19 @@
 window.onerror = function (msg, sauce, lno, cno, er) {
-  document.write(`<p><b>Something went wrong.</b>
+  document.write(`
+  <style>
+  .err {
+    background: rgba(255,155,155,.5);
+    border: solid 2px rgb(255,155,155);
+    border-radius: 15px;
+    font-family: Segoe Print;
+  }
+  </style>
+
+  <p class="err">Something went wrong.
   <br>
-  Remember this page is generated, no content here<br>On line ${lno}:${cno} in ${sauce}
+  Remember this page is generated, no content here
+  <br>
+  On line ${lno}:${cno} in ${sauce}
   <br>
   ${msg}</p>`);
 }
